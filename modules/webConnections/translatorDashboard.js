5422f297cddc7b0544ddf069af531b4cb799c8a8
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-01-30 19:51:21 -0800

    Added check for blacklist and terms not being accepted

--- modules/webConnections/translatorDashboard.js
+++ modules/webConnections/translatorDashboard.js
@@ -5,7 +5,13 @@ module.exports = async (client, clientSecret) => {
 
     //Get user data
     let userData = await models.users.findOne({ clientSecret });
-    if (!userData) return;
+    if (!userData) return new Error("Invalid client secret");
+
+    //Blacklisted
+    if (userData.translator.blacklisted) return new Error("Blacklisted");
+
+    //Terms not accepted
+    if (!userData.translator.acceptedTerms) return new Error("Terms not accepted");
 
     //
 
