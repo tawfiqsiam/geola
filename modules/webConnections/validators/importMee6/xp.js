8afebcf92297824682c01a2ec6e6ca50d114b803
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-02-03 17:40:45 -0800

    Added support for level rewards, xp gain range, and xp gain cooldown

--- modules/webConnections/validators/importMee6/xp.js
+++ modules/webConnections/validators/importMee6/xp.js
@@ -1,21 +1,20 @@
-module.exports = async (client, importMee6, { server }) => {
+module.exports = async (client, xp, { server }) => {
 
     //Pre Module
     const { models, _ } = client.modules.misc.preModule(client);
     const fetch = require("node-fetch");
 
+    //No xp import
+    if (!xp) return;
+
     //Mee6 not on server
     if (!await _.promise(server.fetchMember("159985870458322944"), true)) return;
 
     //Loop through pages
-    let levelRewards;
     for (let i = 0; i < server.memberCount; i++) {
 
         //Fetch data
         const data = await (await fetch(`https://mee6.xyz/api/plugins/levels/leaderboard/${server.id}?page=${i}`)).json();
-
-        //Set level rewards
-        if (i === 0) levelRewards = data.role_rewards.map(rr => ({ level: rr.rank, role: rr.role.id }));
 
         //Loop through members
         for (let j = 0; j < data.players.length; j++) {
