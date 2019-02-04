c56296c09c567c86f15d7ea263a8585a78292d58
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-02-03 16:36:16 -0800

    Added support for importing XP

--- modules/webConnections/validators/importMee6/main.js
+++ modules/webConnections/validators/importMee6/main.js
@@ -1,10 +1,43 @@
 module.exports = async (client, importMee6, { server }) => {
 
     //Pre Module
-    const { _ } = client.modules.misc.preModule(client);
+    const { models, _ } = client.modules.misc.preModule(client);
+    const fetch = require("node-fetch");
 
     //Mee6 not on server
     if (!await _.promise(server.fetchMember("159985870458322944"), true)) return;
 
-    //Import mee6
+    //Loop through pages
+    let levelRewards;
+    for (let i = 0; i < server.memberCount; i++) {
+
+        //Fetch data
+        const data = await (await fetch(`https://mee6.xyz/api/plugins/levels/leaderboard/${server.id}?page=${i}`)).json();
+
+        //Set level rewards
+        if (i === 0) levelRewards = data.role_rewards.map(rr => ({ level: rr.rank, role: rr.role.id }));
+
+        //Loop through members
+        for (let j = 0; j < data.players.length; j++) {
+
+            //Get member
+            let member = data.players[j];
+
+            //Get member data
+            let memberData = await models.members.findById({ server: server.id, user: member.id });
+            if (!memberData) continue;
+
+            let level = member.level;
+            let totalXP = 0;
+            for (let k = 0; k < level; k++) totalXP = totalXP + ((k * 50) + 200);
+
+            //Member xp higher
+            if (memberData.xp.totalXP >= totalXP) continue;
+
+            await models.members.findByIdAndUpdate(
+                { server: server.id, user: member.id },
+                { xp: { xp: 0, level, totalXP } }
+            );
+        }
+    }
 };
