1c19593a1a4d0d91296281b5432081fbc3690426
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-02-17 13:26:24 -0800

    `webConnections/profile` now sends error for invalid user

--- modules/webConnections/profile.js
+++ modules/webConnections/profile.js
@@ -1,11 +1,20 @@
-module.exports = async (client, id) => {
+module.exports = async (client, id, clientSecret) => {
 
     //Pre Module
     const { models } = client.modules.misc.preModule(client);
 
+    //Get user ID via client secret
+    if (!id) {
+
+        const userData = await models.users.findOne({ clientSecret });
+        if (!userData) return { error: "Invalid client secret" };
+
+        id = userData._id;
+    }
+
     //Get user
     const user = client.users.get(id);
-    if (!user) return null;
+    if (!user) return { error: "Invalid user" };
 
     //Get data
     const data = await models.users.findById(id, "xp cubits reputation", { lean: true });
