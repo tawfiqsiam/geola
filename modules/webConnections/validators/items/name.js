0f4ed72fcf20360a451a8041975f572a986b1bd5
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-06-04 17:28:26 -0700

    Fixed bug with deleting or renaming items not updating member invs

--- modules/webConnections/validators/items/name.js
+++ modules/webConnections/validators/items/name.js
@@ -1,4 +1,7 @@
-module.exports = (client, { id, data: name, allData }, { server }) => {
+module.exports = async (client, { id, data: name, allData }, { server }) => {
+
+    //Pre Module
+    const { models, _ } = client.modules.misc.preModule(client);
 
     //Name not changed
     if (id === name) return;
@@ -18,4 +21,14 @@ module.exports = (client, { id, data: name, allData }, { server }) => {
         let item = s.items.find(i => i.name === id);
         if (item) item.name = name;
     });
+
+    //Edit name in member invs
+    const members = await models.members.find({ "_id.server": server.id, "inv.name": id });
+    let memberDocSaves = [];
+    members.forEach(m => {
+        let item = m.inv.find(i => i.name === id);
+        item.name = name;
+        memberDocSaves.push(_.save(client, m));
+    });
+    await Promise.all(memberDocSaves);
 };
--- modules/webConnections/validators/items/remove.js
+++ modules/webConnections/validators/items/remove.js
@@ -1,4 +1,7 @@
-module.exports = (client, name, { server }) => {
+module.exports = async (client, name, { server }) => {
+
+    //Pre Module
+    const { models, _ } = client.modules.misc.preModule(client);
 
     //Get item
     const item = server.data.items.find(i => i === name);
@@ -14,4 +17,13 @@ module.exports = (client, name, { server }) => {
         let item = s.items.find(i => i.name === name);
         if (item) s.items.splice(s.items.indexOf(item), 1);
     });
+
+    //Remove from member invs
+    const members = await models.members.find({ "_id.server": server.id, "inv.name": name });
+    let memberDocSaves = [];
+    members.forEach(m => {
+        m.inv.splice(m.inv.indexOf(m.inv.find(i => i.name === name)), 1);
+        memberDocSaves.push(_.save(client, m));
+    });
+    await Promise.all(memberDocSaves);
 };
